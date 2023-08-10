f81e6024653414a2800151965fc4900b786acb58
Ibrahem Khalil <vampirekid017@gmail.com>
2023-08-10 09:19:47 +0300

    feat: Account overview component (#16742)

--- src/quo2/core_spec.cljs
+++ src/quo2/core_spec.cljs
@@ -53,6 +53,7 @@
     [quo2.components.tags.--tests--.status-tags-component-spec]
     [quo2.components.tags.network-tags.component-spec]
     [quo2.components.tags.status-tags-component-spec]
+    [quo2.components.wallet.account-overview.component-spec]
     [quo2.components.wallet.account-card.component-spec]
     [quo2.components.wallet.network-amount.component-spec]
     [quo2.components.wallet.network-bridge.component-spec]
