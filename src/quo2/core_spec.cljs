4f178ae83c176a0a00eae1f2e9aaf5b2f37e6982
mmilad75 <55688834+mmilad75@users.noreply.github.com>
2023-08-26 18:33:10 +0330

    Implement Data Item component (#16960)
    Implement Data Item component #16960

--- src/quo2/core_spec.cljs
+++ src/quo2/core_spec.cljs
@@ -55,6 +55,7 @@
     [quo2.components.settings.reorder-item.component-spec]
     [quo2.components.settings.settings-list.component-spec]
     [quo2.components.settings.category.component-spec]
+    [quo2.components.settings.data-item.component-spec]
     [quo2.components.share.share-qr-code.component-spec]
     [quo2.components.tags.--tests--.status-tags-component-spec]
     [quo2.components.tags.network-tags.component-spec]
